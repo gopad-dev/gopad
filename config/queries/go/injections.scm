2d15c85c852c33f8550905a3eae4225c4c859d4f
topi314 <git@topi.wtf>
2024-06-24 00:21:51 +0200

    golang highlight sql strings in query methods

--- config/queries/go/injections.scm
+++ config/queries/go/injections.scm
@@ -30,3 +30,15 @@
                 (interpreted_string_literal) @injection.content))
   (#any-of? @_method "Fprintf" "Fscanf" "Appendf" "Sscanf")
   (#set! injection.language "printf"))
+
+(call_expression
+  (selector_expression) @_function
+  (#match? @_function "^[a-zA-Z_][a-zA-Z0-9_]*.(Query|Exec|Prepare|Get|Select)(Context)?$")
+  (argument_list
+    .
+    [
+      (raw_string_literal)
+      (interpreted_string_literal)
+    ] @injection.content
+	(#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "sql")))
