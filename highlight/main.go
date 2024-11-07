b749543ada53393e0ef89ca5a63e3e3dd8281d7d
topi314 <git@topi.wtf>
2024-11-07 21:30:06 +0100

    working highlighter

--- highlight/main.go
+++ highlight/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"log"
-	"time"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
 
 	"go.gopad.dev/gopad/gopad/config"
 )
@@ -16,26 +19,75 @@ func main() {
 }
 `)
 
-func main() {
-	highlighter := NewHighlighter()
+//go:embed config/*
+var defaultConfigs embed.FS
 
-	cfg, err := NewHighlightConfig("go", config.GrammarConfig{}, embed.FS{})
+func main() {
+	if err := config.Load(configDir, defaultConfigs); err != nil {
+		log.Panicln("failed to load config:", err)
+	}
+
+	cfg, err := NewHighlightConfig("go", config.GrammarConfig{
+		Name:       "go",
+		SymbolName: "go",
+		QueriesDir: "config/queries/go",
+		Path:       "config/grammars/libtree-sitter-go.so",
+	}, embed.FS{})
 	if err != nil {
 		log.Fatalf("failed to create highlight config: %v", err)
 	}
+	if cfg == nil {
+		log.Fatalf("tree-sitter grammar not found")
+	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	highlights := highlighter.Highlight(ctx, *cfg, source, nil)
+	highlighter := NewHighlighter()
+	highlights := highlighter.Highlight(context.Background(), *cfg, source, func(name string) *HighlightConfig {
+		log.Println("loading highlight config for", name)
+		return nil
+	})
 
-	for event := range highlights {
+	var highlightName *string
+	for event, err := range highlights {
+		if err != nil {
+			log.Panicf("failed to highlight source: %v", err)
+		}
 		switch e := event.(type) {
 		case HighlightSource:
-			log.Printf("highlighting source from %d to %d", e.Start, e.End)
+			var style lipgloss.Style
+			if highlightName != nil {
+				style = getMatchingStyle(*highlightName, cfg.LanguageName)
+			}
+
+			print(style.Render(string(source[e.Start:e.End])))
+
 		case HighlightStart:
-			log.Printf("starting highlight with style %v", e.Highlight)
+			highlightName = (*string)(&e.Highlight)
 		case HighlightEnd:
-			log.Printf("ending highlight")
+			highlightName = nil
 		}
 	}
+}
+
+func getMatchingStyle(matchType string, languageName string) lipgloss.Style {
+	var currentStyle lipgloss.Style
+
+	for {
+		codeStyle, ok := config.Theme.CodeStyles[fmt.Sprintf("%s.%s", matchType, languageName)]
+		if ok {
+			currentStyle = codeStyle
+			break
+		}
+		codeStyle, ok = config.Theme.CodeStyles[matchType]
+		if ok {
+			currentStyle = codeStyle
+			break
+		}
+		lastDot := strings.LastIndex(matchType, ".")
+		if lastDot == -1 {
+			break
+		}
+		matchType = matchType[:lastDot]
+	}
+
+	return currentStyle
 }
